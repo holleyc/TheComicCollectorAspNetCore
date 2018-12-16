0f978eedf624d064a8d152c420219b4119fb0457
holleyc <holleyc7@me.com>
2018-12-16 00:27:15 -0500

    Added in app.UseDefaultFiles to serve the index.html file in the root of the website.

--- ComicAspNetCore/Startup.cs
+++ ComicAspNetCore/Startup.cs
@@ -15,6 +15,7 @@ namespace ComicAspNetCore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseDefaultFiles();
             app.UseStaticFiles();
         }
     }
