7ded2bc2bfcac5c4cd19d25008d01a2c7b2e004d
holleyc <holleyc7@me.com>
2018-12-16 00:22:42 -0500

    Added the ability to serve files

--- ComicAspNetCore/Startup.cs
+++ ComicAspNetCore/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ComicAspNetCore
@@ -16,10 +15,7 @@ namespace ComicAspNetCore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
         }
     }
 }
