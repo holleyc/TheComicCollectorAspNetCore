14611f2e4d43af11859ca0ddd465ab988a9f0ae6
holleyc <holleyc7@me.com>
2018-12-17 16:06:11 -0500

    Adding in a few more views

--- ComicAspNetCore/Controllers/AppController.cs
+++ ComicAspNetCore/Controllers/AppController.cs
@@ -4,9 +4,37 @@ namespace ComicAspNetCore.Controllers
 {
     public class AppController : Controller
     {
+        //This is the Index view
         public IActionResult Index()
         {
+            return View();
+        }
 
+        //This is the contacts view
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+            return View();
+        }
+
+        //This is the about view
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+            return View();
+        }
+
+        //This is the list all comics view
+        public IActionResult ListAllComics()
+        {
+            ViewBag.Title = "List All Comics";
+            return View();
+        }
+
+        //This is the edit comic view
+        public IActionResult EditComic()
+        {
+            ViewBag.Title = "Edit Comic";
             return View();
         }
     }
