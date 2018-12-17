662e7553a1d7e787f41ec03ed92fc0b1146f86ea
holleyc <holleyc7@me.com>
2018-12-16 21:48:39 -0500

    Enabling MVC 6

---
+++ ComicAspNetCore/Controllers/AppController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ComicAspNetCore.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
--- ComicAspNetCore/Startup.cs
+++ ComicAspNetCore/Startup.cs
@@ -10,15 +10,27 @@ namespace ComicAspNetCore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         // Order matters here
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseStaticFiles();
             app.UseNodeModules(env);
+
+            app.UseMvc(cfg =>
+            {
+                cfg.MapRoute("Default",
+                    "/{controller}/{action}/{id?}",
+                    new { controller = "App", Action = "Index" });
+            });
         }
     }
 }
